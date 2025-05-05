6e12afe3b463ad2c6fde4deae5ffb3fb3ee2960c
Touka-Agra <touka.agra@gmail.com>
2025-05-06 00:16:11 +0300

    Splash Screen

--- .dart_tool/flutter_build/a327e2bc7951582fc82f56d960e7895b/dart_build.d
+++
@@ -1,1 +0,0 @@
- /home/abdelaziz/Desktop/CofeeApp/.dart_tool/flutter_build/a327e2bc7951582fc82f56d960e7895b/dart_build_result.json: 
--- .dart_tool/flutter_build/a9999ec8d56b61bf00a3a9c08324337e/install_code_assets.d
+++
@@ -1,1 +0,0 @@
- /home/abdelaziz/Desktop/VirtualStudyRoom/.dart_tool/flutter_build/a9999ec8d56b61bf00a3a9c08324337e/native_assets.json: 
