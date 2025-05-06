cc07f0ab9e202ec3577a6200f7d8c1d0cf1e6ddd
Abdelaziz Ali <122082712+abdelaziz2642004@users.noreply.github.com>
2025-05-06 18:58:07 +0300

    Delete Zentry-main directory

--- Zentry-main/lib/core/functions.dart
+++
@@ -1,9 +0,0 @@
-String capitalizeFirstLetterOfEachWord(String input) {
-  return input
-      .split(' ')
-      .map((word) {
-        if (word.isEmpty) return word; // Skip empty words
-        return word[0].toUpperCase() + word.substring(1).toLowerCase();
-      })
-      .join(' ');
-}
