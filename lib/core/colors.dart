98bac21ee68ec0feb561b451a096ebf3eb25d7d7
Touka-Agra <touka.agra@gmail.com>
2025-05-09 06:52:53 +0300

    update color palette

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -1,3 +1,30 @@
 import 'package:flutter/material.dart';
 
-const Color mainColor = Color(0xff1b8df7);
+//const Color mainColor = Color(0xff1b8df7);
+//const Color greyShadow = Colors.white38;
+//const Color lightGrey = Color(0xFFF2F2F2);
+//const Color darkText = Color(0xFF2D2D2D);
+//const Color shadowColor = Colors.black26;
+//const Color whiteColor = Colors.white;
+//2e374a,b7bac1,7c5659,947575,ebe1e1,8a6463,b59892,7b7b85,546472,dce4e9
+
+const Color mainColor = Color(0xFF3b4b62);
+const Color darkMainColor = Color(0xFF37475e);
+const Color lightMainColor = Color(0xFFdce4e9);
+
+const Color secondaryColor = Color(0xFF8a6463);
+const Color darkSecondaryColor = Color(0xFF7c5659);
+const Color lightSecondaryColor = Color(0xFFebe1e1);
+
+const Color grey = Color(0xFFb7bac1);
+const Color darkGrey = Color(0xFF7b7b85);
+const Color lightGrey = Color(0xFFb9c0c8);
+
+const Color white = Colors.white;
+const Color black = Colors.black;
+
+const Color light = Colors.white70;
+const Color dark = Colors.black38;
+
+
+
