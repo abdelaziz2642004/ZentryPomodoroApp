ff0ff445a33af3270045819c1da7e6dece3ae3ff
abdelaziz2642004 <abdelaziz2004264@gmail.com>
2025-05-09 14:21:16 +0300

    fixed timer late initialization for phase logic

--- lib/View/Screens/RoomScreen/RoomScreen.dart
+++ lib/View/Screens/RoomScreen/RoomScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:prj/View/Screens/RoomScreen/HelpingWidgets/RoomPhaseTimer.dart';
 import 'package:prj/ViewModel/Cubits/Auth/Auth_cubit.dart';
 import 'package:prj/ViewModel/Cubits/RoomOperations/Room_Cubit.dart';
 import 'package:prj/ViewModel/Cubits/RoomOperations/Room_States.dart';
@@ -224,89 +225,6 @@ class RoomScreen extends StatelessWidget {
           );
         },
       ),
-    );
-  }
-}
-
-class RoomPhaseTimer extends StatefulWidget {
-  final DateTime createdAt;
-  final int workDuration;
-  final int breakDuration;
-  final int totalSessions;
-
-  const RoomPhaseTimer({
-    Key? key,
-    required this.createdAt,
-    required this.workDuration,
-    required this.breakDuration,
-    required this.totalSessions,
-  }) : super(key: key);
-
-  @override
-  State<RoomPhaseTimer> createState() => _RoomPhaseTimerState();
-}
-
-class _RoomPhaseTimerState extends State<RoomPhaseTimer> {
-  late Timer _timer;
-  late int elapsedMinutes;
-  String currentPhase = "";
-  int timeInPhase = 0;
-
-  void updatePhase() {
-    final now = DateTime.now();
-    final elapsed = now.difference(widget.createdAt);
-    elapsedMinutes = elapsed.inMinutes;
-
-    final fullCycle = widget.workDuration + widget.breakDuration;
-    final cyclesPassed = elapsedMinutes ~/ fullCycle;
-    final remTime = elapsedMinutes % fullCycle;
-
-    if (cyclesPassed >= widget.totalSessions) {
-      setState(() {
-        currentPhase = "Finished all sessions";
-        timeInPhase = 0;
-      });
-      _timer.cancel(); // stop timer when done
-      return;
-    }
-
-    if (remTime < widget.workDuration) {
-      setState(() {
-        currentPhase = "Work";
-        timeInPhase = remTime;
-      });
-    } else {
-      setState(() {
-        currentPhase = "Break";
-        timeInPhase = remTime - widget.workDuration;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    updatePhase();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      updatePhase();
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("Current Phase: $currentPhase"),
-        Text("Minutes in Current Phase: $timeInPhase"),
-        Text("Elapsed Time: $elapsedMinutes minutes"),
-      ],
     );
   }
 }
