8641847ef4c63cfec0c2b4381448f1477c3ea002
Touka-Agra <touka.agra@gmail.com>
2025-05-09 13:38:40 +0300

    make color, shadow, fontweight optional Parameters

--- lib/View/Widgets/CreateRoomDialog/HelpingWidgets/FormText.dart
+++ lib/View/Widgets/CreateRoomDialog/HelpingWidgets/FormText.dart
@@ -4,8 +4,17 @@ import '../../../../core/colors.dart';
 
 class FormTextTitle extends StatelessWidget {
   final String text;
+  final FontWeight fontWeight;
+  final Color color;
+  final Color shadow;
 
-  const FormTextTitle({super.key, required this.text});
+  const FormTextTitle({
+    super.key,
+    required this.text,
+    this.fontWeight = FontWeight.w600,
+    this.color = white,
+    this.shadow = darkMainColor,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +22,15 @@ class FormTextTitle extends StatelessWidget {
       alignment: Alignment.centerLeft,
       child: Text(
         text,
-        style: const TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 16,
-            color: white,
-            shadows: [Shadow(color: darkMainColor, blurRadius: 3, offset: Offset(1,2))]
+        style: TextStyle(
+          fontWeight: fontWeight,
+          fontSize: 16,
+          color: color,
+          shadows: [
+            Shadow(color: shadow, blurRadius: 3, offset: const Offset(1, 2)),
+          ],
         ),
       ),
     );
   }
 }
-
