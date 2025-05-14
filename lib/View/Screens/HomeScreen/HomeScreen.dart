bddb5bfdc1b977ea3d4f664ab7c6ce060158f7d5
abdelaziz2642004 <abdelaziz2004264@gmail.com>
2025-05-14 23:54:28 +0300

    prevented the ability to join another room in the homescreen while already revisiting a another after internet disconnection

--- lib/View/Screens/HomeScreen/HomeScreen.dart
+++ lib/View/Screens/HomeScreen/HomeScreen.dart
@@ -10,6 +10,7 @@ import 'package:prj/View/Screens/HomeScreen/HelpingWidgets/TimeTrackerToday.dart
 import 'package:prj/View/Screens/RoomScreen/RoomScreen.dart';
 import 'package:prj/ViewModel/Cubits/Auth/Auth_cubit.dart';
 import 'package:prj/ViewModel/Cubits/RoomOperations/Room_Cubit.dart';
+import 'package:prj/ViewModel/Cubits/RoomOperations/Room_States.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -48,57 +49,66 @@ class _HomescreenState extends State<Homescreen> {
       drawer: const Customdrawer(),
       appBar: Customappbar.build(),
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Timetrackertoday(),
+        child: BlocBuilder<RoomCubit, RoomStates>(
+          builder: (context, state) {
+            if (state is RoomJoinLoadingState) {
+              return const CircularProgressIndicator();
+            }
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Timetrackertoday(),
 
-              ElevatedButton(
-                onPressed: () async {
-                  final id = BlocProvider.of<AuthCubit>(context).user!.id;
-                  PomodoroRoom room = PomodoroRoom(
-                    name: "bgrbbbb",
-                    workDuration: 1,
-                    breakDuration: 5,
-                    totalSessions: 4,
-                    createdAt: Timestamp.now(),
-                    creatorId: id,
-                    availableRoom: true,
-                    capacity: 43,
-                    isPublic: true,
-                    tags: ["test", "room"],
-                    joinedUsers: [],
-                  );
-                  await BlocProvider.of<RoomCubit>(context).createRoom(room);
-                },
-                child: const Text("Create Random Test Room"),
-              ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final id = BlocProvider.of<AuthCubit>(context).user!.id;
+                      PomodoroRoom room = PomodoroRoom(
+                        name: "bgrbbbb",
+                        workDuration: 1,
+                        breakDuration: 5,
+                        totalSessions: 4,
+                        createdAt: Timestamp.now(),
+                        creatorId: id,
+                        availableRoom: true,
+                        capacity: 43,
+                        isPublic: true,
+                        tags: ["test", "room"],
+                        joinedUsers: [],
+                      );
+                      await BlocProvider.of<RoomCubit>(
+                        context,
+                      ).createRoom(room);
+                    },
+                    child: const Text("Create Random Test Room"),
+                  ),
 
-              const SizedBox(height: 24),
-              const Text(
-                "Recently Joined",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  const SizedBox(height: 24),
+                  const Text(
+                    "Recently Joined",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  const Recentlyjoined(),
+                  const SizedBox(height: 24),
+                  const Text(
+                    "Public Rooms",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 12),
+                  const RoomsGridBuilder(),
+                  const SizedBox(height: 24),
+                  Center(
+                    child: Text(
+                      "Avatar & Creator",
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 8),
-              const Recentlyjoined(),
-              const SizedBox(height: 24),
-              const Text(
-                "Public Rooms",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 12),
-              const RoomsGridBuilder(),
-              const SizedBox(height: 24),
-              Center(
-                child: Text(
-                  "Avatar & Creator",
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
