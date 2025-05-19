35372145859c69b41028ae2c871d7e78b85a97b7
Touka-Agra <touka.agra@gmail.com>
2025-05-19 10:00:03 +0300

    fetch creatorImage

--- lib/View/Widgets/RoomGridItems/HelpingWidgets/CreatorInfo.dart
+++ lib/View/Widgets/RoomGridItems/HelpingWidgets/CreatorInfo.dart
@@ -1,23 +1,106 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:prj/core/colors.dart';
 
 class CreatorInfo extends StatelessWidget {
-  const CreatorInfo({
-    super.key,
-    required this.creatorName,
-  });
+  const CreatorInfo({super.key, required this.creatorId});
 
-  final String creatorName;
+  final String creatorId;
+
+  Future<String> fetchCreatorName(String creatorId) async {
+    try {
+      final doc =
+          await FirebaseFirestore.instance
+              .collection('Users')
+              .doc(creatorId)
+              .get();
+
+      if (doc.exists) {
+        final data = doc.data();
+        if (data != null && data.containsKey('username')) {
+          return data['username'] as String;
+        }
+      }
+    } catch (e) {
+      print("Error fetching username: $e");
+    }
+    return "Unknown";
+  }
+
+  Future<String?> fetchCreatorImage(String creatorId) async {
+    try {
+      final doc =
+          await FirebaseFirestore.instance
+              .collection('Users')
+              .doc(creatorId)
+              .get();
+
+      if (doc.exists) {
+        final data = doc.data();
+        if (data != null && data.containsKey('imageUrl')) {
+          String imageUrl = data['imageUrl'].toString();
+          return imageUrl == ""? null : imageUrl;
+        }
+      }
+    } catch (e) {
+      print("Error fetching Profile Image: $e");
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        CircleAvatar(radius: 10, backgroundColor: Colors.grey[300]),
+        CircleAvatar(
+          backgroundColor: grey,
+          radius: 12,
+          child: FutureBuilder<String?>(
+            future: fetchCreatorImage(creatorId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SizedBox(
+                  width: 20,
+                  height: 20,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2,
+                    color: mainColor,
+                  ),
+                );
+              }
+
+              if (snapshot.hasData && snapshot.data != null) {
+                return CircleAvatar(
+                  radius: 10,
+                  backgroundColor: Colors.grey[300],
+                  backgroundImage: CachedNetworkImageProvider(snapshot.data!),
+                );
+              }
+
+              return const CircleAvatar(
+                radius: 10,
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.person, size: 12, color: Colors.white),
+              );
+            },
+          ),
+        ),
         const SizedBox(width: 4),
-        Text(
-          "by $creatorName",
-          style: const TextStyle(fontSize: 10, color: Colors.black54),
+        FutureBuilder<String?>(
+          future: fetchCreatorName(creatorId),
+          builder: (context, snapshot) {
+            final creatorName =
+                snapshot.connectionState == ConnectionState.done &&
+                        snapshot.hasData
+                    ? snapshot.data!
+                    : "Loading...";
+
+            return Text(
+              "by $creatorName",
+              style: const TextStyle(fontSize: 10, color: Colors.black54),
+            );
+          },
         ),
       ],
     );
--- lib/View/Widgets/RoomGridItems/RoomGridItem.dart
+++ lib/View/Widgets/RoomGridItems/RoomGridItem.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:prj/Models/PomodoroRoom.dart';
-import 'package:prj/core/colors.dart';
 
 import '../HelpingWidgets/CustomContainer.dart';
 import 'HelpingWidgets/ControlBadge.dart';
@@ -19,24 +17,6 @@ class RoomGridItem extends StatelessWidget {
 
   RoomGridItem({super.key, required this.room});
 
-  Future<String> fetchCreatorName(String creatorId) async {
-    try {
-      final doc = await FirebaseFirestore.instance
-          .collection('Users')
-          .doc(creatorId)
-          .get();
-
-      if (doc.exists) {
-        final data = doc.data();
-        if (data != null && data.containsKey('username')) {
-          return data['username'] as String;
-        }
-      }
-    } catch (e) {
-      print("Error fetching username: $e");
-    }
-    return "Unknown";
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,79 +30,74 @@ class RoomGridItem extends StatelessWidget {
     final int roomCapacity = room.capacity;
     final int numberOfSessions = room.totalSessions;
 
+    return Stack(
+      children: [
+        CustomContainer(
+          color: Colors.white,
+          blurRadius: 4,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 10,
+              right: 10,
+              top: 18,
+              bottom: 12,
+            ),
+            child: Column(
+              //mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /// Room Name
+                RoomName(roomName: roomName),
+                const SizedBox(height: 4),
 
-    return FutureBuilder<String?>(
-      future: fetchCreatorName(creatorId),
-      builder: (context, snapshot) {
-        final creatorName =
-            snapshot.connectionState == ConnectionState.done && snapshot.hasData
-                ? snapshot.data!
-                : "Loading...";
-        return Stack(
-          children: [
-            CustomContainer(
-              color: Colors.white,
-              blurRadius: 4,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10, top: 18, bottom: 12),
-                child: Column(
-                  //mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                /// Creator Info
+                CreatorInfo(creatorId: creatorId),
+                const SizedBox(height: 8),
+
+                /// Sessions Info
+                SessionInfo(numberOfSessions: numberOfSessions),
+                const SizedBox(height: 12),
+
+                /// Work & Break Time
+                WorkBreakView(
+                  time: workTime,
+                  icon: Icons.work,
+                  iconColor: Colors.blueAccent,
+                  title: "Work Time: ",
+                ),
+                const SizedBox(height: 4),
+                WorkBreakView(
+                  time: breakTime,
+                  icon: Icons.coffee,
+                  iconColor: Colors.orange,
+                  title: "Break Time: ",
+                ),
+
+                /// Tags
+                Tags(tags: tags),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    /// Room Name
-                    RoomName(roomName: roomName),
-                    const SizedBox(height: 4),
-
-                    /// Creator Info
-                    CreatorInfo(creatorName: creatorName),
-                    const SizedBox(height: 8),
-
-                    /// Sessions Info
-                    SessionInfo(numberOfSessions: numberOfSessions,),
-                    const SizedBox(height: 12),
-
-                    /// Work & Break Time
-                    WorkBreakView(
-                      time: workTime,
-                      icon: Icons.work,
-                      iconColor: Colors.blueAccent,
-                      title: "Work Time: ",
+                    /// Joined Users
+                    JoinedUsers(
+                      joinedUsers: joinedUsers,
+                      roomCapacity: roomCapacity,
                     ),
-                    const SizedBox(height: 4),
-                    WorkBreakView(
-                      time: breakTime,
-                      icon: Icons.coffee,
-                      iconColor: Colors.orange,
-                      title: "Break Time: ",
-                    ),
+                    const SizedBox(width: 15),
 
-                    /// Tags
-                    Tags(tags: tags),
-
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        /// Joined Users
-                        JoinedUsers(
-                          joinedUsers: joinedUsers,
-                          roomCapacity: roomCapacity,
-                        ),
-                        const SizedBox(width: 15),
-
-                        /// Join Button
-                        const JoinButton(),
-                      ],
-                    ),
+                    /// Join Button
+                    const JoinButton(),
                   ],
                 ),
-              ),
+              ],
             ),
+          ),
+        ),
 
-            /// Control Badge
-            ControlBadge(isPublic: isPublic),
-          ],
-        );
-      },
+        /// Control Badge
+        ControlBadge(isPublic: isPublic),
+      ],
     );
   }
 }
